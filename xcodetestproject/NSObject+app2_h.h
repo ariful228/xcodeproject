2ca213ff398ffce5619d2e30e6d3eae037c6818d
Ariful Islam <ariful@arifuls-mac-3.local>
2019-09-17 03:23:46 -0700

    Test file app2 add

---
+++ xcodetestproject/NSObject+app2_h.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+app2_h.h
+//  xcodetestproject
+//
+//  Created by Ariful Islam on 17/9/19.
+//  Copyright © 2019 bjitbjit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (app2_h)
+
+@end
---
+++ xcodetestproject/NSObject+app2_h.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+app2_h.m
+//  xcodetestproject
+//
+//  Created by Ariful Islam on 17/9/19.
+//  Copyright © 2019 bjitbjit. All rights reserved.
+//
+
+#import "NSObject+app2_h.h"
+
+@implementation NSObject (app2_h)
+
+@end
