5117afcb17980fa185b1f7ee38a0858c49d8dde0
Ariful Islam <ariful228@outlook.com>
2019-09-16 23:25:19 -0700

    xcode commit from tarminal

---
+++ xcodetestproject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xcodetestproject
+//
+//  Created by  on 15/9/19.
+//  Copyright © 2019 bjitbjit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ xcodetestproject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  xcodetestproject
+//
+//  Created by  on 15/9/19.
+//  Copyright © 2019 bjitbjit. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
